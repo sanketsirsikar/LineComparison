10006d1a22368460dcd585d3b5ed9a9d62f0b202
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-21 12:45:56 +0530

    Line comparison probleam

---
+++ bridgelabz/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparison {
+    public static void main (String[]args){
+        System.out.println("Welcome to Line comparison Computation program");
+    }
+}
